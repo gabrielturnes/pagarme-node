6ebb43fc21f343e240a6248112f82e6f99fee4cb
Gabriel <gabriel_turnes@live.com>
2020-08-03 18:28:05 -0300

    primeiro commit

---
+++ bin/base/repository-base.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+class baseRepository {
+  constructor(model) {
+    this._model = mongoose.model(model);
+  }
+  async create(data) {
+    let modelo = new this._model(data);
+    let resultado = await modelo.save();
+    return resultado;
+  }
+  async update(id, data, usuarioLogado) {
+    await this._model.findByIdAndUpdate(id, {
+      $set: data
+    });
+    let resultado = await this._model.findById(id);
+    return resultado;
+  }
+  async getAll() {
+    return await this._model.find({});
+  }
+  async getMyAll(user) {
+    return await this._model.find({
+      userId: user._id
+    });
+  }
+  async delete(id) {
+    return await this._model.findByIdAndDelete(id);
+  }
+  async getById(id) {
+    return await this._model.findById(id);
+  }
+}
+
+module.exports = baseRepository;
